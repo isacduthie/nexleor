8be6b1f1a191c12e5a043fb8b74326716538f9bc
Daniel Isaac <danielbcbs2@gmail.com>
2026-01-03 15:31:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/nexleor/",
+  base: "./",
   build: {
     assetsDir: "assets",
     outDir: "dist",
